4e1e4ffc16113531e877b5b1a7d876841058c4e1
1101137149 <fgd8389@gmail.com>
2015-03-31 16:05:11 +0800

    update

--- WebApplication1/Class2.cs
+++ WebApplication1/Class2.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-using NUnit.Framework;
+//using NUnit.Framework;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -10,12 +10,13 @@ using System.Data.SqlClient;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace WebApplication1
 {
     public class Class2
     {
 
-        [Test]
+        //[Test]
         public void CalYear() {
             WebApplication1.Cal cal = new WebApplication1.Cal();
             int inputAge = 30;
